66ed573e6cbf2d02127ac5e51275fadf66575690
Naivira <eddiemir1991@gmail.com>
2025-07-30 14:13:21 -0700

    Add firebaseConfig.js with Firebase configuration

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBYkqaKTcsTl17oxAcGGDiV_GwZx-ucNi8",
+  authDomain: "naivira-b14ed.firebaseapp.com",
+  projectId: "naivira-b14ed",
+  storageBucket: "naivira-b14ed.appspot.com",
+  messagingSenderId: "904644471061",
+  appId: "1:904644471061:web6c4eba35de79b21751611c",
+  measurementId: "G-QNNNN3YJTJ"
+};
